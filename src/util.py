c3d91fd4235066107b6c7ab226d64924452cbcb6
Yasuhiro Osajima <kimukimu33@gmail.com>
2023-03-05 06:12:59 +0900

    WIP: add server code

--- src/util.py
+++ src/util.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-from typing import Union
+from typing import Optional, Union
 
 from fastapi import Depends,  HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
@@ -14,6 +14,7 @@ from model import TokenData, User, UserInDB
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "RS512"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+TIME_DELTA_MINUTES = 15
 
 fake_users_db = {
     "testuser": {
@@ -31,63 +32,84 @@ pwd_context = CryptContext(schemes=["bcrypt"],
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
+def get_password_hash(password) -> str:
     return pwd_context.hash(password)
 
 
-def get_user(db, username: str):
+def verify_password(plain_password,
+                    hashed_password) -> bool:
+    return pwd_context.verify(plain_password,
+                              hashed_password)
+
+
+def _get_user(db, username: str) -> Optional[UserInDB]:
+    user_in_db = None
+
     if username in db:
         user_dict = db[username]
-        return UserInDB(**user_dict)
+        user_in_db = UserInDB(**user_dict)
+
+    return user_in_db
 
 
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
+def authenticate_user(username: str,
+                      password: str) -> Optional[UserInDB]:
+    user = _get_user(fake_users_db,
+                     username)
+
     if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
+        return None
+
+    if not verify_password(password,
+                           user.hashed_password):
+        return None
+
     return user
 
 
 def create_access_token(data: dict,
-                        expires_delta: Union[timedelta, None] = None):
+                        expires_delta: Union[timedelta, None] = None) -> str:
     to_encode = data.copy()
+
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=TIME_DELTA_MINUTES)
+
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
     return encoded_jwt
 
 
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str = Depends(oauth2_scheme)) -> UserInDB:
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
+
         token_data = TokenData(username=username)
     except JWTError:
         raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
+
+    user = _get_user(fake_users_db, username=token_data.username)
+
     if user is None:
         raise credentials_exception
+
     return user
 
 
 async def get_current_active_user(
-        current_user: User = Depends(get_current_user)):
+        current_user: User = Depends(get_current_user)) -> User:
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
+
     return current_user
